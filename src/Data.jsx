52809a41f50314622626bf2dafeb183aede4cdfb
E-Begich <128361003+E-Begich@users.noreply.github.com>
2023-06-12 09:18:12 +0200

    .

--- src/Data.jsx
+++ src/Data.jsx
@@ -4,7 +4,7 @@ const DATA = [
         title : "Amra",
         price : 70,
         desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
-        img : "/images/amra.jpg"
+        img : "/public/images/amra.jpg"
     },
     {
         id : 1,
