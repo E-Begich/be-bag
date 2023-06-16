ad0b90debd0d2e6ffd8a2270acbb2d7538ab45e7
E-Begich <128361003+E-Begich@users.noreply.github.com>
2023-06-16 22:10:21 +0200

    pokusaj 202

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,30 +4,30 @@ import Product from './Product';
 const Home = () => {
   return (
     <div>
-      <div id="carouselExampleIndicators" className="carousel slide">
-        <div className="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <div id='carouselExampleIndicators' className='carousel slide'>
+        <div className='carousel-indicators'>
+          <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='0' className='active' aria-current='true' aria-label='Slide 1'></button>
+          <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='1' aria-label='Slide 2'></button>
+          <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='2' aria-label='Slide 3'></button>
         </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src="https://e-begich.github.io/be-bag/assets/images/bg.jpg" className="d-block w-100" alt="background1" height="700px"/>
+        <div className='carousel-inner'>
+          <div className='carousel-item active'>
+            <img src='../../public/assets/images/bg.jpg' className='d-block w-100' alt='background1' height='700px'/>
           </div>
-          <div className="carousel-item">
-            <img src="/assets/images/sabrina.jpg" className="d-block w-100" alt="background2" height="700px"/>
+          <div className='carousel-item'>
+            <img src='/assets/images/sabrina.jpg' className='d-block w-100' alt='background2' height='700px'/>
           </div>
-          <div className="carousel-item">
-            <img src="/assets/images/kristina.jpg" className="d-block w-100" alt="background3"height="700px"/>
+          <div className='carousel-item'>
+            <img src='/assets/images/kristina.jpg' className='d-block w-100' alt='background3'height='700px'/>
           </div>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
+        <button className='carousel-control-prev' type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide='prev'>
+          <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+          <span className='visually-hidden'>Previous</span>
         </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
+        <button className='carousel-control-next' type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide='next'>
+          <span className='carousel-control-next-icon' aria-hidden='true'></span>
+          <span className='visually-hidden'>Next</span>
         </button>
       </div>
       <p className='lead text-center py-4 my-6'>This is test site. Please do not order for now.</p>
