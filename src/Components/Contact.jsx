8e3d81358ca1b2badc34255f3598ac265b5a8251
E-Begich <128361003+E-Begich@users.noreply.github.com>
2023-08-02 12:12:43 +0200

    env2

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
         </div>
         <div className="row">
           <div className="col-md-5 d-flex justify-content-center">
-            <img src="/assets/contactus.gif" alt="contact us" height="500px" width="600px" />
+            <img src={process.env.PUBLIC_URL+ "/assets/contactus.gif"} alt="contact us" height="500px" width="600px" />
           </div>
           <div className="col-md-6">
             <form>
